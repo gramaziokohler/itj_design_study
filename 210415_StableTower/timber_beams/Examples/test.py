66f3e24b93d29de276a821ef3dd98c8be0c8dae2
Victor Leung <yck011522@gmail.com>
2021-08-03 19:09:22 +0200

    Tower and Hut scheme based on a stability first consideration

---
+++ 210415_StableTower/timber_beams/Examples/test.py
@@ -0,0 +1,9 @@
+lengths = [3, 2, 5, 1]
+
+sorted_length = sorted(range(len(lengths)), key=lambda x:lengths[x])
+
+# for i in range(len(lengths)):
+#     print(i)
+
+print(lengths[:2])
+print(lengths[2:])
