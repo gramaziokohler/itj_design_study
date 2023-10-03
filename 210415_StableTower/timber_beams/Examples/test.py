697efff57627a35c611c565166f86a76a5dd7371
Victor LEUNG <yck011522@gmail.com>
2023-10-03 12:51:21 +0800

    Removed non-model related files

--- 210415_StableTower/timber_beams/Examples/test.py
+++
@@ -1,9 +0,0 @@
-lengths = [3, 2, 5, 1]
-
-sorted_length = sorted(range(len(lengths)), key=lambda x:lengths[x])
-
-# for i in range(len(lengths)):
-#     print(i)
-
-print(lengths[:2])
-print(lengths[2:])
